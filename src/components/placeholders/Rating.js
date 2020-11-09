551ab08d7b1c01ffca8b561abcc0a7629f52dc9d
Imran Ahmad <imran@concernbox.com>
2020-11-09 19:42:39 +0530

    Made rating placeholder shorter

--- src/components/placeholders/Rating.js
+++ src/components/placeholders/Rating.js
@@ -3,7 +3,7 @@ import { Placeholder } from "semantic-ui-react";
 
 const RatingPlaceholder = props => (
   <Placeholder>
-    <Placeholder.Line length='medium' />
+    <Placeholder.Line length="very short" />
   </Placeholder>
 );
 
